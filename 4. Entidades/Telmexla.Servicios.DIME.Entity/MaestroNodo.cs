e356680bcff84a42bc5ad6f29c45fb94bb0f4251
David1010Gomez <dagomez.15m@gmail.com>
2017-04-07 10:49:58 -0500

    Commit

--- 4. Entidades/Telmexla.Servicios.DIME.Entity/MaestroNodo.cs
+++ 4. Entidades/Telmexla.Servicios.DIME.Entity/MaestroNodo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,22 +10,51 @@ namespace Telmexla.Servicios.DIME.Entity
     //[TBL_INFORMACION_NODOS]
     public class  MaestroNodo
     {
-        public decimal IdNodo { get; set; } 
+        public decimal IdNodo { get; set; }
+
+        [Required(ErrorMessage = "Este campo no puede ser vacio")]
         public string Nodo { get; set; }
+
+        [Required(ErrorMessage = "Este campo no puede ser vacio")]
         public string NombreNodo { get; set; }
-        public string Div { get; set; } 
-        public string Com { get; set; } 
+
+        [Required(ErrorMessage = "Este campo no puede ser vacio")]
+        public string Div { get; set; }
+
+        [Required(ErrorMessage = "Este campo no puede ser vacio")]
+        public string Com { get; set; }
+
+        [Required(ErrorMessage = "Este campo no puede ser vacio")]
         public string Divisional { get; set; }
+
+        [Required(ErrorMessage = "Este campo no puede ser vacio")]
         public string Area { get; set; }
+
+        [Required(ErrorMessage = "Este campo no puede ser vacio")]
         public string Zona { get; set; }
+
+        [Required(ErrorMessage = "Este campo no puede ser vacio")]
         public string Distrito { get; set; }
+
+        [Required(ErrorMessage = "Este campo no puede ser vacio")]
         public string Ugestion { get; set; }
+
         public string Usuario { get; set; }
-        public System.DateTime? FechaCreacion { get; set; } 
-        public string Estado { get; set; } 
-        public string Red { get; set; } 
+        public System.DateTime? FechaCreacion { get; set; }
+
+        [Required(ErrorMessage = "Debe Seleccionar una Opción")]
+        public string Estado { get; set; }
+
+        [Required(ErrorMessage = "Este campo no puede ser vacio")]
+        public string Red { get; set; }
+
+        [Required(ErrorMessage = "Este campo no puede ser vacio")]
         public string Aliado { get; set; }
+
+        [Required(ErrorMessage = "Este campo no puede ser vacio")]
         public string NombreComunidad { get; set; }
+
+        [Required(ErrorMessage = "Este campo no puede ser vacio")]
         public string Departamento { get; set; }
 
     }
