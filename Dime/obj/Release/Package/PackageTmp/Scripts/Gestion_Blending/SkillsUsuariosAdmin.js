8844ef816120807d336f509666e0eb8c26042cb2
Robinson Neusa Pinilla <robinson.neusa@gmail.com>
2017-04-25 10:54:34 -0500

    OTRO COMMIT

---
+++ Dime/obj/Release/Package/PackageTmp/Scripts/Gestion_Blending/SkillsUsuariosAdmin.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () {
+    $("#cuentaCliente").prop("readonly", true);
+    $("#Li1").click(function () {
+
+        $("#Li2").css('background-color', 'transparent');
+        $("#Li1").css("background-color", "#dcdcdc");
+        $("#Li1").css("border-color", "#c23321");
+        $("#Li2").css("border-color", "transparent");
+
+    });
+
+    $("#Li2").click(function () {
+        $("#Li1").css("background-color", "transparent");
+        $("#Li2").css("background-color", "#dcdcdc");
+        $("#Li1").css("border-color", "transparent");
+        $("#Li2").css("border-color", "#c23321");
+
+    });
+
+});
