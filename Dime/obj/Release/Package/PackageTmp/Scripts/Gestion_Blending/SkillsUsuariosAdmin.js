ac1046514218343d69ea5a685550d6f4c8d3ce47
Robinson Neusa Pinilla <robinson.neusa@gmail.com>
2017-04-26 15:37:23 -0500

    commit

--- Dime/obj/Release/Package/PackageTmp/Scripts/Gestion_Blending/SkillsUsuariosAdmin.js
+++ Dime/obj/Release/Package/PackageTmp/Scripts/Gestion_Blending/SkillsUsuariosAdmin.js
@@ -16,5 +16,122 @@
         $("#Li2").css("border-color", "#c23321");
 
     });
-
+    TraerListaLineasBlending();
 });
+
+function TraerListaLineasBlending() {
+    $.ajax({
+        type: "GET",
+        url: urlLineas,
+        dataType: "JSON",
+        success: function (result) {
+            var json = JSON.parse(result);
+            for (var index = 0; index < json.length; index++) {
+                $('#NombreLineasBlendingSelect').append($('<option>', {
+                    value: json[index].NombreLinea,
+                    text: json[index].NombreLinea
+                }));
+
+            }
+
+        },
+        error: function (request, status, error) {
+            alert(request.responseText);
+        }
+    });
+}
+
+function BuscaUsuariosForList()
+{
+   var Operacion =  $('#NombreLineasBlendingSelect').val();
+   $.ajax({
+       type: "POST",
+       url: urlUsuariosporOperacion,
+       contentType: "application/json; charset=utf-8",
+       data: JSON.stringify({ Operacion: Operacion }),
+       dataType: "JSON",
+       success: function (result) {
+           var json = JSON.parse(result);
+           ShowGridUsuarios(json);
+           $('#oculto').css("display","block");
+       }
+   });
+}
+
+
+function SetearCierresChoices() {
+    var IdContacto = $('#tipoContactosSelect').val();
+    $.ajax({
+        type: "POST",
+        url: urlControlTiposCierresList,
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify({ idContacto: IdContacto }),
+        dataType: "JSON",
+        success: function (result) {
+            var json = JSON.parse(result);
+            for (var index = 0, len = json.length; index < len; index++) {
+                $('#tiposCierresSelect').append($('<option>', {
+                    value: json[index].IdCierre,
+                    text: json[index].Cierre
+                }));
+
+            }
+
+        }
+    });
+
+
+    $('#tiposCierresSelect').find('option:not(:first)').remove();
+    $('#tiposRazonSelect').find('option:not(:first)').remove();
+    $('#tiposCausasSelect').find('option:not(:first)').remove();
+    $('#tiposMotivosSelect').find('option:not(:first)').remove();
+}
+
+function ShowGridUsuarios(data)
+{
+    $("#gridViewResult").kendoGrid({
+
+        //toolbar: ["excel"],
+        //excel: {
+        //    fileName: "Export.xlsx",
+        //},
+        dataSource: {
+            data: data,
+            pageSize: 10,
+
+        },
+
+        scrollable: true,
+        filterable: {
+            extra: false,
+            operators: {
+                string: {
+                    eq: "Es igual a"
+                }
+            }
+        },
+        sortable: true,
+        pageable: {
+            refresh: true,
+            pageSizes: true,
+            buttonCount: 5
+        },
+        columns: [
+            { text: "Check", template: '<input type="checkbox" id="ARadio"/>', title: "Check", width: "70px" },
+            { field: "Id", title: "Id", width: 80, headerAttributes: { style: "white-space: normal" } },
+            { field: "Cedula", title: "Id Usuario", width: 80, headerAttributes: { style: "white-space: normal" } },
+            { field: "Operacion", title: "Operacion", width: 80, headerAttributes: { style: "white-space: normal" } },
+            { field: "Campaña", title: "Campaña", width: 80, headerAttributes: { style: "white-space: normal" } },
+
+        ]
+
+    });
+}
+
+$('#cedulaCreacion').on("keyup", function (e) {
+    var code = e.keyCode || e.which;
+    if (code == 13) {
+        $("#consultarUsuario").click();
+
+    }
+})
--- Dime/obj/Release/Package/PackageTmp/Scripts/Traslados/ConsultaAdminIngresosTraslados.js
+++ Dime/obj/Release/Package/PackageTmp/Scripts/Traslados/ConsultaAdminIngresosTraslados.js
@@ -1,69 +1,4 @@
-﻿function cambiarfechas() {
-    for (var i = 0; i < jsonconsultaadminit.length; i++) {
-        jsonconsultaadminit[i].IngresoTrasladoGetSet.FechaApertura = kendo.toString(kendo.parseDate(jsonconsultaadminit[i].IngresoTrasladoGetSet.FechaApertura, 'yyyy-MM-dd'), 'yyyy-MM-dd');
-        jsonconsultaadminit[i].IngresoTrasladoGetSet.HoraApertura = kendo.toString(kendo.parseDate(jsonconsultaadminit[i].IngresoTrasladoGetSet.HoraApertura, 'HH:mm:ss'), 'HH:mm:ss');
-        jsonconsultaadminit[i].IngresoTrasladoGetSet.FechaCierre = kendo.toString(kendo.parseDate(jsonconsultaadminit[i].IngresoTrasladoGetSet.FechaCierre, 'yyyy-MM-dd'), 'yyyy-MM-dd');
-        jsonconsultaadminit[i].IngresoTrasladoGetSet.HoraCierre = kendo.toString(kendo.parseDate(jsonconsultaadminit[i].IngresoTrasladoGetSet.HoraCierre, 'HH:mm:ss'), 'HH:mm:ss');
-        jsonconsultaadminit[i].IngresoTrasladoGetSet.FechaUltimaActualizacion = kendo.toString(kendo.parseDate(jsonconsultaadminit[i].IngresoTrasladoGetSet.FechaUltimaActualizacion, 'yyyy-MM-dd'), 'yyyy-MM-dd');
-        jsonconsultaadminit[i].IngresoTrasladoGetSet.HoraUltimaActualizacion = kendo.toString(kendo.parseDate(jsonconsultaadminit[i].IngresoTrasladoGetSet.HoraUltimaActualizacion, 'HH:mm:ss'), 'HH:mm:ss');
-    }
-        
-}
-if (jsonconsultaadminit != null)
-{
-  
-    cambiarfechas();
-
-}
-
-
-$("#gridViewConsulta").kendoGrid({
-    autoBind: true,
-    toolbar: ["excel"],
-    excel: {
-        fileName: "Export.xlsx",
-    },
-    dataSource: {
-        data: jsonconsultaadminit,
-        pageSize: 20,
-    },
-    scrollable: true,
-    filterable: {
-        extra: false,
-        operators: {
-            string: {
-
-                eq: "Es igual a"
-            }
-        }
-    },
-    sortable: true,
-    pageable: {
-        refresh: true,
-        pageSizes: true,
-        buttonCount: 5
-    },
-    columns: [
-    { field: "IngresoTrasladoGetSet.IdTransaccion", title: "Id Transacción", width: 100 },
-    { field: "IngresoTrasladoGetSet.CuentaCliente", title: "Cuenta Cliente", width: 100 },
-    { field: "IngresoTrasladoGetSet.TipoGestion", title: "Tipo Gestion", width: 100 },
-    { field: "IngresoTrasladoGetSet.FechaApertura", title: "Fecha Apertura", width: 100, filterable: false },
-    { field: "IngresoTrasladoGetSet.HoraApertura", title: "Hora Apertura", width: 100,filterable: false },
-    { field: "IngresoTrasladoGetSet.UsuarioApertura", title: "Usuario Apertura", width: 100 },
-    { field: "IngresoTrasladoGetSet.FechaCierre", title: "Fecha Cierre", width: 100, filterable: false },
-    { field: "IngresoTrasladoGetSet.HoraCierre", title: "Hora Cierre", width: 100, filterable: false },
-    { field: "IngresoTrasladoGetSet.UsuarioCierre", title: "Usuario Cierre", width: 100 },
-    { field: "IngresoTrasladoGetSet.FechaUltimaActualizacion", title: "Fecha Actualizacion", width: 100,  filterable: false },
-    { field: "IngresoTrasladoGetSet.HoraUltimaActualizacion", title: "Hora Actualizacion", width: 100,  filterable: false },
-    { field: "IngresoTrasladoGetSet.UltimaActualizacion", title: "Usuario Actualizacion", width: 100 },
-    { field: "IngresoTrasladoGetSet.EstadoTransaccion", title: "Estado Transaccion", width: 100 },
-    { field: "IngresoTrasladoGetSet.AliadoApertura", title: "Aliado Apertura", width: 100 },
-    { field: "IngresoTrasladoGetSet.NombreLineaIngreso", title: "Linea Ingreso", width: 100 },
-    { field: "IngresoTrasladoGetSet.NombreLineaEscalado", title: "Linea Escalado", width: 100 }
-    ]
-
-});
-
+﻿
 $.datetimepicker.setLocale('es');
 $('#Fecha_Inicial').datetimepicker({
     format: 'Y-m-d',
@@ -90,16 +25,96 @@ $("#Fecha_Final").blur(function (event) {
         $("#warningLabel").text("Debe primero introducir una fecha inicial");
     } else {
 
-        HacerConsulta();
+        
     }
     console.log("cambio en vacio " + fechaInicial);
-
-
+    var F1 = $('#Fecha_Inicial').val();
+    var F2 = $('#Fecha_Final').val();
+   
+    TraerDatosConsulta(F1,F2);
 });
-function HacerConsulta() {
 
-    var bt1 = document.getElementById("submitDatos");
-    bt1.click();
-    
+
+function TraerDatosConsulta(F1,F2) {
+    document.getElementById('dataLoading').style.display = 'inline-block';
+    $.ajax({
+        type: "GET",
+        url: urlListatraslados,
+        contentType: "application/json; charset=utf-8",
+        data: { fechaInicial: F1, fechaFinal: F2 },
+        dataType: "JSON",
+        success: function (result) {
+            var json = JSON.parse(result);
+            cambiarfechas(json);
+            cargargridview(json);
+            finalizaconsulta();
+        },
+        error: function (request, status, error) {
+            alert(request.responseText);
+        }
+    });
 }
+function cambiarfechas(data) {
+    for (var i = 0; i < data.length; i++) {
+        data[i].IngresoTrasladoGetSet.FechaApertura = kendo.toString(kendo.parseDate(data[i].IngresoTrasladoGetSet.FechaApertura, 'yyyy-MM-ddTHH:mm:ss'), 'yyyy-MM-dd');
+        data[i].IngresoTrasladoGetSet.HoraApertura = kendo.toString(kendo.parseDate(data[i].IngresoTrasladoGetSet.HoraApertura, 'yyyy-MM-ddTHH:mm:ss'), 'HH:mm:ss');
+        data[i].IngresoTrasladoGetSet.FechaCierre = kendo.toString(kendo.parseDate(data[i].IngresoTrasladoGetSet.FechaCierre, 'yyyy-MM-ddTHH:mm:ss'), 'yyyy-MM-dd');
+        data[i].IngresoTrasladoGetSet.HoraCierre = kendo.toString(kendo.parseDate(data[i].IngresoTrasladoGetSet.HoraCierre, 'yyyy-MM-ddTHH:mm:ss'), 'HH:mm:ss');
+        data[i].IngresoTrasladoGetSet.FechaUltimaActualizacion = kendo.toString(kendo.parseDate(data[i].IngresoTrasladoGetSet.FechaUltimaActualizacion, 'yyyy-MM-dd'), 'yyyy-MM-dd');
+        data[i].IngresoTrasladoGetSet.HoraUltimaActualizacion = kendo.toString(kendo.parseDate(data[i].IngresoTrasladoGetSet.HoraUltimaActualizacion, 'yyyy-MM-ddTHH:mm:ss'), 'HH:mm:ss');
+        
+    }
 
+}
+function cargargridview(data) {
+    $("#gridViewConsulta").kendoGrid({
+        autoBind: true,
+        toolbar: ["excel"],
+        excel: {
+            fileName: "Export.xlsx",
+        },
+        dataSource: {
+            data: data,
+            pageSize: 20,
+        },
+        scrollable: true,
+        filterable: {
+            extra: false,
+            operators: {
+                string: {
+
+                    eq: "Es igual a"
+                }
+            }
+        },
+        sortable: true,
+        pageable: {
+            refresh: true,
+            pageSizes: true,
+            buttonCount: 5
+        },
+        columns: [
+        { field: "IngresoTrasladoGetSet.IdTransaccion", title: "Id Transacción", width: 100 },
+        { field: "IngresoTrasladoGetSet.CuentaCliente", title: "Cuenta Cliente", width: 100 },
+        { field: "IngresoTrasladoGetSet.TipoGestion", title: "Tipo Gestion", width: 100 },
+        { field: "IngresoTrasladoGetSet.FechaApertura", title: "Fecha Apertura", width: 100, filterable: false },
+        { field: "IngresoTrasladoGetSet.HoraApertura", title: "Hora Apertura", width: 100, filterable: false },
+        { field: "IngresoTrasladoGetSet.UsuarioApertura", title: "Usuario Apertura", width: 100 },
+        { field: "IngresoTrasladoGetSet.FechaCierre", title: "Fecha Cierre", width: 100, filterable: false },
+        { field: "IngresoTrasladoGetSet.HoraCierre", title: "Hora Cierre", width: 100, filterable: false },
+        { field: "IngresoTrasladoGetSet.UsuarioCierre", title: "Usuario Cierre", width: 100 },
+        { field: "IngresoTrasladoGetSet.FechaUltimaActualizacion", title: "Fecha Actualizacion", width: 100, filterable: false },
+        { field: "IngresoTrasladoGetSet.HoraUltimaActualizacion", title: "Hora Actualizacion", width: 100, filterable: false },
+        { field: "IngresoTrasladoGetSet.UsuarioUltimaActualizacion", title: "Usuario Actualizacion", width: 100 },
+        { field: "IngresoTrasladoGetSet.EstadoTransaccion", title: "Estado Transaccion", width: 100 },
+        { field: "IngresoTrasladoGetSet.AliadoApertura", title: "Aliado Apertura", width: 100 },
+        { field: "IngresoTrasladoGetSet.NombreLineaIngreso", title: "Linea Ingreso", width: 100 },
+        { field: "IngresoTrasladoGetSet.NombreLineaEscalado", title: "Linea Escalado", width: 100 }
+        ]
+
+    });
+   
+}
+function finalizaconsulta() {
+    document.getElementById('dataLoading').style.display = 'none';
+}
