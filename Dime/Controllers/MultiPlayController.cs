7a6bf9c1ae21b7b626a0ce90743b74ccc480025d
David1010Gomez <dagomez.15m@gmail.com>
2017-06-28 14:52:15 -0500

    Commit

---
+++ Dime/Controllers/MultiPlayController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Dime.Controllers
+{
+    public class MultiPlayController : Controller
+    {
+        public ActionResult MultiPlayCelula()
+        {
+            return View();
+        }
+    }
+}
