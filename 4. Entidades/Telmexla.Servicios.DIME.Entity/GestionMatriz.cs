13119cee1561132ccf3bcdbf50534539697d379f
Robinson Neusa Pinilla <robinson.neusa@gmail.com>
2017-06-29 07:38:16 -0500

    Commit Matrices

--- 4. Entidades/Telmexla.Servicios.DIME.Entity/GestionMatriz.cs
+++ 4. Entidades/Telmexla.Servicios.DIME.Entity/GestionMatriz.cs
@@ -12,15 +12,20 @@ namespace Telmexla.Servicios.DIME.Entity
         public System.DateTime? FechaTransaccion { get; set; } // FECHA DE LA TRANSACCION
         public string NombreLineaTransaccion { get; set; } // NOMBRE DE LA LINEA DE LA TRANSACCION
         public string TipoGestionMatriz { get; set; } //TIPO GESTION MATRIZ
+        [StringLength(5, ErrorMessage = "Este campo no debe ser superior a 5 caracteres.")]
         public string TipoCliente { get; set; } //TIPO CLIENTE
         public decimal CuentaCliente { get; set; } // CUENTA DEL CLIENTE
         public decimal CuentaMatriz { get; set; } // CUENTA MATRIZ
         public decimal OrdenTrabajo { get; set; } // ORDEN DE TRABAJO
         public string Direccion { get; set; } //DIRECCION A CREAR
         public string Nodo { get; set; } // NODO
+        [StringLength(100, ErrorMessage = "Este campo no debe ser superior a 100 caracteres.")]
         public string NombreConjuntoEdificio { get; set; } // NOMBRE CONJUNTO O EDIFICIO
+        [StringLength(20, ErrorMessage = "Este campo no debe ser superior a 20 caracteres.")]
         public string TelefonoCLiente { get; set; } // TELEFONO DEL CLIENTE
+        [StringLength(20, ErrorMessage = "Este campo no debe ser superior a 20 caracteres.")]
         public string TelefonoAdministrador { get; set; }// TELEFONO DEL ADMINMISTRADOR
+        [StringLength(100, ErrorMessage = "Este campo no debe ser superior a 100 caracteres.")]
         public string NombreAdministrador { get; set; }// TELEFONO FIJO DEL CLIENTE
         public string Razon { get; set; }// RAZON
        
