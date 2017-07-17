f36cfd3709d4a5c9d8d4aad50aba84712f80c847
Robinson Neusa Pinilla <robinson.neusa@gmail.com>
2017-07-17 14:05:12 -0500

    Merge branch 'master' of https://github.com/robineusa/Dime_App

---
+++ Dime/Controllers/CierreDeCicloController.cs
@@ -0,0 +1,44 @@
+﻿using Dime.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Dime.Controllers
+{
+    [ExpiringFilter]
+    public class CierreDeCicloController : MyController
+    {
+
+        public CierreDeCicloController()
+        {
+
+        }
+
+        // GET: CierreDeCiclo
+        public ActionResult ResidencialPredictivo()
+        {
+            return View("ConsultaGestiones");
+        }
+
+        public ActionResult ResidencialPotencial()
+        {
+            return View("ConsultaGestiones");
+        }
+
+        public ActionResult Instalacion()
+        {
+            return View("ConsultaGestiones");
+        }
+
+        public ActionResult Pymes()
+        {
+            return View();
+        }
+
+
+
+
+    }
+}
