12029045a9d6367fb84669fbcebf7915f9cfefd4
Robinson Neusa Pinilla <robinson.neusa@gmail.com>
2017-07-18 12:01:53 -0500

    Admin Mec - Correos

--- Dime/Scripts/Mec/AdministrarLineasMec.js
+++ Dime/Scripts/Mec/AdministrarLineasMec.js
@@ -1,14 +1,16 @@
 ﻿$(document).ready(function () {
-    ListaProcesos();
-
+    ListaLineas();
+  
 });
 
-function ListaProcesos() {
+function ListaLineas() {
     document.getElementById('dataLoading').style.display = 'inline-block';
+    var Id = $('#idproceso').val();
     $.ajax({
         type: "GET",
-        url: ListaProcesosMecAdmin,
+        url: ListaLineasPorProceso,
         contentType: "application/json; charset=utf-8",
+        data: { IdProceso: Id},
         dataType: "JSON",
         success: function (result) {
             var json = JSON.parse(result);
@@ -45,9 +47,11 @@ function cargargrilla(data) {
         },
         columns: [
         { command: { text: " ", click: ActualizarProceso, imageClass: "k-icon k-i-pencil", }, title: "Editar", width: "60px" },
+        { field: "IdLinea", title: "Id Línea", width: 100 },
         { field: "IdProceso", title: "Id Proceso", width: 100 },
-        { field: "Proceso", title: "Nombre del Proceso", width: 100 },
-        { field: "Estado", title: "Estado", width: 100 }
+        { field: "NombreLinea", title: "Nombre de la Línea", width: 100 },
+        { field: "Estado", title: "Estado", width: 100 },
+        { command: { text: "Correos", click: ListaDistribucion, imageClass: "k-icon k-i-mail", }, title: "Lista de Distribución", width: "80px" },
         ]
 
     });
@@ -59,6 +63,13 @@ function finalizaconsulta() {
 function ActualizarProceso(e) {
     e.preventDefault();
     var dataItem = this.dataItem($(e.currentTarget).closest("tr"));
-    window.location.href = 'AgregarNuevoProceso?IdProceso=' + dataItem.IdProceso;
+    window.location.href = 'ActualizarLineasMec?IdProceso=' + dataItem.IdProceso + '&IdLinea=' + dataItem.IdLinea;
 
 };
+function ListaDistribucion(e) {
+    e.preventDefault();
+    var dataItem = this.dataItem($(e.currentTarget).closest("tr"));
+    window.location.href = 'ListasdeDistribucion?IdProceso=' + dataItem.IdProceso + '&IdLinea=' + dataItem.IdLinea;
+
+};
+
