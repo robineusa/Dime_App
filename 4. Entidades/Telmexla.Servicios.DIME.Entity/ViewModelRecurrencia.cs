2abc6faa7a4abeff8bf2daa41420695ccf2ecafd
David1010Gomez <dagomez.15m@gmail.com>
2017-07-25 11:45:19 -0500

    Recurrencia

---
+++ 4. Entidades/Telmexla.Servicios.DIME.Entity/ViewModelRecurrencia.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telmexla.Servicios.DIME.Entity
+{
+    public class ViewModelRecurrencia
+    {
+        private ClientesTodo clientesTodos;
+
+        public ViewModelRecurrencia()
+        {
+            ClientesTodos = new ClientesTodo();
+        }
+
+        public ClientesTodo ClientesTodos
+        {
+            get
+            {
+                return clientesTodos;
+            }
+
+            set
+            {
+                clientesTodos = value;
+            }
+        }
+    }
+}
