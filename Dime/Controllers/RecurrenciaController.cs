2b9f53c3ded9b64c45703dc847886db4fe72b95f
David1010Gomez <dagomez.15m@gmail.com>
2017-07-25 11:32:50 -0500

    Commit

---
+++ Dime/Controllers/RecurrenciaController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Dime.Controllers
+{
+    public class RecurrenciaController : Controller
+    {
+        public ActionResult Recurrencia()
+        {
+            return View();
+        }
+    }
+}
