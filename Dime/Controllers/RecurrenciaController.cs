1f54865dca3eeeb9d333ecbab75b129a5832e9f5
David1010Gomez <dagomez.15m@gmail.com>
2017-07-25 16:14:23 -0500

    Recurrencia

--- Dime/Controllers/RecurrenciaController.cs
+++ Dime/Controllers/RecurrenciaController.cs
@@ -1,16 +1,34 @@
-﻿using System;
+﻿using Dime.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Telmexla.Servicios.DIME.Entity;
 
 namespace Dime.Controllers
 {
     public class RecurrenciaController : Controller
     {
+        WSD.InboundServiceClient inboundService;
+
+        public RecurrenciaController()
+        {
+            inboundService = new WSD.InboundServiceClient();
+            inboundService.ClientCredentials.Authenticate();
+        }
+        [HttpGet]
         public ActionResult Recurrencia()
         {
-            return View();
+            ViewModelRecurrencia model = new ViewModelRecurrencia();
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Recurrencia(ViewModelRecurrencia model, string BotonEnvia)
+        {
+            model.ClientesTodos = inboundService.TraerClienteCompletoPorCuenta(model.ClientesTodos.Cuenta);
+            return View(model);
+            
         }
     }
 }
