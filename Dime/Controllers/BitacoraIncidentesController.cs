58899835493730f9208087c7d5348fd3d2a9b481
Robinson Neusa Pinilla <robinson.neusa@gmail.com>
2017-08-14 15:10:02 -0500

    Commit Bitacora

--- Dime/Controllers/BitacoraIncidentesController.cs
+++ Dime/Controllers/BitacoraIncidentesController.cs
@@ -24,5 +24,31 @@ namespace Dime.Controllers
             ViewModelBitacoraIncidentes modelo = new ViewModelBitacoraIncidentes();
             return View(modelo);
         }
+        [HttpPost]
+        public ActionResult RegistrodeIncidentes(ViewModelBitacoraIncidentes modelo)
+        {
+            return View();
+        }
+        [HttpGet]
+        public JsonResult ListaHerramientasJson()
+        {
+            var jsonResult = Json(JsonConvert.SerializeObject(bitacoraservice.ListaDeHerramientas()), JsonRequestBehavior.AllowGet);
+            jsonResult.MaxJsonLength = int.MaxValue;
+            return jsonResult;
+        }
+        [HttpGet]
+        public JsonResult ListaTipoDeFallaJson()
+        {
+            var jsonResult = Json(JsonConvert.SerializeObject(bitacoraservice.ListaTiposDeFallas()), JsonRequestBehavior.AllowGet);
+            jsonResult.MaxJsonLength = int.MaxValue;
+            return jsonResult;
+        }
+        [HttpGet]
+        public JsonResult ListaPrioridadesJson()
+        {
+            var jsonResult = Json(JsonConvert.SerializeObject(bitacoraservice.ListaDePrioridades()), JsonRequestBehavior.AllowGet);
+            jsonResult.MaxJsonLength = int.MaxValue;
+            return jsonResult;
+        }
     }
 }
