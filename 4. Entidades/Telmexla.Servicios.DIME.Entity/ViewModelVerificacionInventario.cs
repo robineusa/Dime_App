44e052b8afd829e64f35dbd695e076ebb9ec6774
Robinson Neusa Pinilla <robinson.neusa@gmail.com>
2017-09-29 08:08:17 -0500

    Verificacion de Inventario 2

---
+++ 4. Entidades/Telmexla.Servicios.DIME.Entity/ViewModelVerificacionInventario.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telmexla.Servicios.DIME.Entity
+{
+    public class ViewModelVerificacionInventario
+    {
+        #region Entidades
+
+        VIPSolicitudes vIPSolicitudes;
+        VIPSolicitudesPorEquipo vIPSolicitudesPorEquipo;
+        VILSolicitudes vILSolicitudes;
+        VIMAliadoTecnico vIMAliadoTecnico;
+        VIMGestion vIMGestion;
+        VIMSubrazon vIMSubrazon;
+        VIMTipoDeEquipos vIMTipoDeEquipos;
+        VIMTipoDeRequerimiento vIMTipoDeRequerimiento;
+
+        #endregion
+
+        #region Constructores
+
+        public ViewModelVerificacionInventario()
+        {
+            vIPSolicitudes = new VIPSolicitudes();
+            vIPSolicitudesPorEquipo = new VIPSolicitudesPorEquipo();
+            vILSolicitudes = new VILSolicitudes();
+            vIMAliadoTecnico = new VIMAliadoTecnico();
+            vIMGestion = new VIMGestion();
+            vIMSubrazon = new VIMSubrazon();
+            vIMTipoDeEquipos = new VIMTipoDeEquipos();
+            vIMTipoDeRequerimiento = new VIMTipoDeRequerimiento();
+        }
+
+        #endregion
+
+        #region Encapsulamientos
+
+        public VIPSolicitudes VIPSolicitudes
+        {
+            get
+            {
+                return vIPSolicitudes;
+            }
+
+            set
+            {
+                vIPSolicitudes = value;
+            }
+        }
+
+        public VIPSolicitudesPorEquipo VIPSolicitudesPorEquipo
+        {
+            get
+            {
+                return vIPSolicitudesPorEquipo;
+            }
+
+            set
+            {
+                vIPSolicitudesPorEquipo = value;
+            }
+        }
+
+        public VILSolicitudes VILSolicitudes
+        {
+            get
+            {
+                return vILSolicitudes;
+            }
+
+            set
+            {
+                vILSolicitudes = value;
+            }
+        }
+
+        public VIMAliadoTecnico VIMAliadoTecnico
+        {
+            get
+            {
+                return vIMAliadoTecnico;
+            }
+
+            set
+            {
+                vIMAliadoTecnico = value;
+            }
+        }
+
+        public VIMGestion VIMGestion
+        {
+            get
+            {
+                return vIMGestion;
+            }
+
+            set
+            {
+                vIMGestion = value;
+            }
+        }
+
+        public VIMSubrazon VIMSubrazon
+        {
+            get
+            {
+                return vIMSubrazon;
+            }
+
+            set
+            {
+                vIMSubrazon = value;
+            }
+        }
+
+        public VIMTipoDeEquipos VIMTipoDeEquipos
+        {
+            get
+            {
+                return vIMTipoDeEquipos;
+            }
+
+            set
+            {
+                vIMTipoDeEquipos = value;
+            }
+        }
+
+        public VIMTipoDeRequerimiento VIMTipoDeRequerimiento
+        {
+            get
+            {
+                return vIMTipoDeRequerimiento;
+            }
+
+            set
+            {
+                vIMTipoDeRequerimiento = value;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Dime/Controllers/VerificacionDeInventarioController.cs
@@ -0,0 +1,31 @@
+﻿using Dime.Helpers;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Mvc;
+using Telmexla.Servicios.DIME.Entity;
+
+namespace Dime.Controllers
+{
+    public class VerificacionDeInventarioController : Controller
+    {
+        WSD.VerificacionDeInventarioServiceClient VerificacionInventarioService;
+        WSD.MaestroNodoServiceClient Maestronodosservice;
+        public VerificacionDeInventarioController()
+        {
+            VerificacionInventarioService = new WSD.VerificacionDeInventarioServiceClient();
+            VerificacionInventarioService.ClientCredentials.Authenticate();
+            Maestronodosservice = new WSD.MaestroNodoServiceClient();
+            Maestronodosservice.ClientCredentials.Authenticate();
+        }
+        [HttpGet]
+        public ActionResult RegistrarSolicitud()
+        {
+            ViewModelVerificacionInventario modelo = new ViewModelVerificacionInventario();
+            return View(modelo);
+        }
+    }
+}
