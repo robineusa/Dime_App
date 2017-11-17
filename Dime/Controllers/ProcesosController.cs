c009f1f5fad134ff1696d0e573b3972ad95e9099
David1010Gomez <dagomez.15m@gmail.com>
2017-11-17 16:42:43 -0500

    Commit Procesos

---
+++ Dime/Controllers/ProcesosController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Dime.Controllers
+{
+    public class ProcesosController : Controller
+    {
+        [HttpGet]
+        public ActionResult GestionNoCheck()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult AdminArboles()
+        {
+            return View();
+        }
+
+
+    }
+}
