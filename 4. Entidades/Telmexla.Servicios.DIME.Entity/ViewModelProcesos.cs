f11da35af7c8e8a3dfceaefca88923a346630f8f
Antonio <antony021santana.96@gmail.com>
2017-11-21 09:01:05 -0500

    Commit arbol

---
+++ 4. Entidades/Telmexla.Servicios.DIME.Entity/ViewModelProcesos.cs
@@ -0,0 +1,46 @@
+﻿
+using System.Collections.Generic;
+
+namespace Telmexla.Servicios.DIME.Entity
+{
+    public class ViewModelProcesos
+    {
+        private Nodo nodo;
+        private List<Nodo> listarnodos;
+        public ViewModelProcesos()
+        {
+            nodo = new Nodo();
+            listarnodos = new List<Nodo>();
+        }
+
+        public Nodo Nodo
+        {
+            get
+            {
+                return nodo;
+            }
+
+            set
+            {
+                nodo = value;
+            }
+        }
+
+        public List<Nodo> ListarNodos
+        {
+            get
+            {
+                return listarnodos;
+            }
+
+            set
+            {
+                listarnodos = value;
+            }
+        }
+    }
+}
+
+
+
+
--- Dime/Controllers/ProcesosController.cs
+++ Dime/Controllers/ProcesosController.cs
@@ -1,13 +1,23 @@
-﻿using System;
+﻿using Dime.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Telmexla.Servicios.DIME.Entity;
 
 namespace Dime.Controllers
 {
     public class ProcesosController : Controller
     {
+        WSD.ProcesosServiceClient ProcesosService;
+
+        public ProcesosController()
+        {
+            ProcesosService = new WSD.ProcesosServiceClient();
+            ProcesosService.ClientCredentials.Authenticate();
+        }
+
         [HttpGet]
         public ActionResult GestionNoCheck()
         {
@@ -20,9 +30,24 @@ namespace Dime.Controllers
             return View();
         }
 
+        [HttpGet]
         public ActionResult EditarArbol()
         {
+            ViewModelProcesos vmp = new ViewModelProcesos();
+            vmp.ListarNodos = ProcesosService.ConsultarNodos(1);
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult EditarArbol(string nombreNodo)
+        {
+            Nodo nodo = new Nodo();
+            nodo.IdPadre = 0;
+            nodo.NombreNodo = nombreNodo;
+            nodo.IdArbol = 1;
+            ProcesosService.CrearNodo(nodo);
+            return View();
+
         }
 
     }
