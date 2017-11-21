5f295044e27b8886927bf98bec3e20ae767463c4
Antonio <antony021santana.96@gmail.com>
2017-11-21 15:17:21 -0500

    Commit arbol

--- Dime/Controllers/ProcesosController.cs
+++ Dime/Controllers/ProcesosController.cs
@@ -33,44 +33,13 @@ namespace Dime.Controllers
         [HttpGet]
         public ActionResult EditarArbol()
         {
-            ViewModelProcesos vmp = new ViewModelProcesos();
-            ViewModelProcesos NodosPadres = new ViewModelProcesos();
-            ViewModelProcesos ColeccionVmp = new ViewModelProcesos();
-            ViewModelProcesos Union;
-
-            ColeccionVmp.NombreArbol = "Arbol pruebas";
-            //Consulta los nodos
-            vmp.ListarNodos = ProcesosService.ConsultarNodos(1);
-
-            //Filtra solo los nodos padre
-            NodosPadres.ListarNodos = (from n in vmp.ListarNodos
-                                       where n.IdPadre == 0
-                                       select n).ToList();
-
-            //Agrega los nodos hijos a los padres
-            foreach (var item in NodosPadres.ListarNodos)
-            {
-                Union = new ViewModelProcesos();
-                Union.ListarNodos.Add(item);
-                Union.NodosHijos = (from n in vmp.ListarNodos
-                                    where n.IdPadre != 0 && n.IdPadre == item.Id
-                                    select n).ToList();
-
-                ColeccionVmp.ColeccionVmp.Add(Union);
-            }
-
-            return View(ColeccionVmp);
+            return View();
         }
 
         [HttpPost]
         public ActionResult EditarArbol(string nombreNodo)
         {
-            Nodo nodo = new Nodo();
-            nodo.IdPadre = 0;
-            nodo.NombreNodo = nombreNodo;
-            nodo.IdArbol = 1;
-            ProcesosService.CrearNodo(nodo);
-            EditarArbol();
+          
             return View();
 
         }
