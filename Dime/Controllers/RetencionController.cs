e7c4cfac6912cb9e99c73379eba84e3303955319
Robinson Neusa Pinilla <robinson.neusa@gmail.com>
2017-11-22 08:01:13 -0500

    Retencion

---
+++ Dime/Controllers/RetencionController.cs
@@ -0,0 +1,28 @@
+﻿using Dime.Helpers;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Mvc;
+using Telmexla.Servicios.DIME.Entity;
+
+namespace Dime.Controllers
+{
+    public class RetencionController : Controller
+    {
+        WSD.RetencionServiceClient retencionservice;
+        public RetencionController()
+        {
+            retencionservice = new WSD.RetencionServiceClient();
+            retencionservice.ClientCredentials.Authenticate();
+
+        }
+        [HttpGet]
+        public ActionResult RegistroSetguimientos()
+        {
+            return View();
+        }
+    }
+}
