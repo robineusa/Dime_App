aba90e09e9d699b006071993212d1563a25ab699
Robinson Neusa Pinilla <robinson.neusa@gmail.com>
2017-12-01 06:41:04 -0500

    Post - Publicacion

--- Dime/obj/Release/Package/PackageTmp/Scripts/Procesos/EditarArbol.js
+++ Dime/obj/Release/Package/PackageTmp/Scripts/Procesos/EditarArbol.js
@@ -1,52 +1,108 @@
 ﻿$(document).ready(function () {
     var IdArbol = 1;
+    var IdPadre = 0;
+
     ConstruirArbol(IdArbol);
+
+    //$('#Body_Layout').on('click', function () { });
+
+
+
 });
 
-function ConstruirArbol(idArbol) {
-    $.ajax({
-        type: "POST",
-        url: urlLLamarArbolId,
-        contentType: "application/json; charset=utf-8",
-        data: JSON.stringify({ IDdArbol: idArbol }),
-        dataType: "JSON",
-        success: function (result) {
-            var json = JSON.parse(result);
-            console.log(result);
-            $('#InsertaArbol').append('<ul>' +
-                '<strong>' +
-                    '<i class="fa fa-folder"></i> ' + json.NombreArbol +
-                '</strong>' + json.CodigoHtml + '<i class="fa fa-plus-square"></i><br/>' +
-            '</ul>');
-        },
-        error: function (request, status, error) {
-            alert(request.responseText);
-        }
-    });
+
+//function ConstruirArbol(idArbol) {
+//    $.ajax({
+//        type: "POST",
+//        url: urlLLamarArbolId,
+//        contentType: "application/json; charset=utf-8",
+//        data: JSON.stringify({ IDdArbol: idArbol }),
+//        dataType: "JSON",
+//        success: function (result) {
+//            var json = JSON.parse(result);
+//            console.log(result);
+//            $('#InsertaArbol').append('<div id="NombreArbol" idArb="' + json.Id + '" onclick="return evnt(this)" style="font-weight:bold;"><i class="fa fa-folder"></i>' +
+//                        '&nbsp' + json.NombreArbol + ' '+
+//                        '<a href="#CrearNodo" data-toggle="modal" data-backdrop="static" data-keyboard="false"><i class="fa fa-plus-square-o"></i></a></div>' +
+//                  '<ul id="ulPrincipal">' +
+//                            json.CodigoHtml +
+//                        '</ul>');
+//        },
+//        error: function (request, status, error) {
+//            alert(request.responseText);
+//        }
+//    });
+//}
+
+var contador = 0;
+var contador1 = 0;
+var nodoSeleccionado = {
+    IdPadre: "",
+    Id: ""
 }
+//var IdPadre = 0;
+
 function evnt(objeto) {
 
-    var posicion = objeto.getBoundingClientRect();
-    document.getElementById("lista").style.position = "absolute";
-    document.getElementById("lista").style.display = "block";
-    document.getElementById("lista").style.left = posicion.left + 20 + "px";
-    document.getElementById("lista").style.top = posicion.top + "px";
+    window.event.cancelBubble = true;
+    obj = objeto.parentNode;
+    nodoSeleccionado.IdPadre = obj.id;
+    nodoSeleccionado.Id = objeto.id;
 
 }
 
 function crear() {
-    var obj = document.getElementById("ulPrincipal");
+    if (nodoSeleccionado.IdPadre == "InsertaArbol") {
 
-    var li = document.createElement("li");
-    var txt = document.createTextNode("Primer nodo exitoso");
-    li.appendChild(txt);
-    li.setAttribute("id", "li" + contador);
-    li.setAttribute("onclick", "evnt(this)");
-    obj.appendChild(li);
+        $("#ulPrincipal").append("<i></i><li  onmousedown='return evnt(this)'" + "id= 'li" + contador + "'>"
+           + "<span onmouseover='pruebas(this)' onmouseout='pruebas2(this)'> Primer nodo exitoso  <i onclick='crear()' class='fa fa-plus-circle'></i><i onclick='Eliminar()' class='fa fa-minus-circle'></i></span> </li>")
 
-    contador++;
+        contador++;
+
+    } else {
+        var objeto = document.getElementById(nodoSeleccionado.Id);
+        //alert("Creacion li - " + objeto.id);
+
+        if (objeto.childNodes.length < 3) {
+
+            $(objeto).append("<ul id='ul  ;" + contador1 + "'>" + "<i></i>" + "<li  onmousedown='return evnt(this)'" +
+                           "id= 'li" + contador + "." + contador1 + "'>" + "<span onmouseover='pruebas(this)' onmouseout='pruebas2(this)'> nodo creado <i onclick='crear()' class='fa fa-plus-circle'></i><i onclick='Eliminar()' class='fa fa-minus-circle'></i></span></li> </ul>");
+
+            contador1++;
+
+        } else {
+
+            var ulNivel1 = objeto.childNodes[2];
+            $(ulNivel1).append("<i></i>" + "<li  id='subHijo1." + contador + "-" + contador1 +
+                "'  onmousedown='return evnt(this)'>" +
+                "<span onmouseover='pruebas(this)' onmouseout='pruebas2(this)'>nodo creado<i onclick='crear()' class='fa fa-plus-circle'></i><i onclick='Eliminar()' class='fa fa-minus-circle'></i><span/></li>");
+
+            contador1++;
+
+        }
+
+    }
 }
+function Eliminar() {
 
+    var objeto;
+    objeto = document.getElementById(nodoSeleccionado.Id);
+    var objPadre = objeto.parentNode;
+
+    var cont = objPadre.childNodes.length;
+    //alert(objPadre.getAttribute("id") + "-" + cont);
+    var numeral;
+    for (f = 0; f < cont; f++) {
+        if (objPadre.childNodes[f].nodeType == Node.ELEMENT_NODE) {
+
+            if (objPadre.childNodes[f].id == objeto.getAttribute("id")) {
+                numeral = f;
+
+            }
+        }
+    }
+    objPadre.removeChild(objPadre.childNodes[numeral]);
+}
 function ocultar() {
     document.getElementById("lista").style.display = "none";
 
@@ -57,6 +113,27 @@ function poner() {
 
 }
 
+
+
+
+function ocultar() {
+    document.getElementById("Menu").style.display = "none";
+
+}
+
+function poner() {
+    document.getElementById("Menu").style.display = "block";
+
+}
+
 function seleccionado() {
-    document.getElementById("lista").style.display = "none";
+    document.getElementById("Menu").style.display = "block";
+}
+
+function pruebas(obj) {
+    obj.style.backgroundColor = "#336699";
+}
+
+function pruebas2(obj) {
+    obj.style.backgroundColor = "";
 }
