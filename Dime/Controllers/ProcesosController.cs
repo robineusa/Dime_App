7828dee5ecffa907631f087f4723a16e425f472c
Antonio <antony021santana.96@gmail.com>
2017-12-06 11:06:13 -0500

    commit

--- Dime/Controllers/ProcesosController.cs
+++ Dime/Controllers/ProcesosController.cs
@@ -88,7 +88,7 @@ namespace Dime.Controllers
 
             model.Id = Convert.ToInt32(IDdArbol);
             model.CodigoHtml = CodigoHTML;
-            //ProcesosService.ActualizarCodigoArbol(model);
+            ProcesosService.ActualizaHTMLArbol(model);
 
             var jsonResult = Json(JsonConvert.SerializeObject("Proceso Exitoso"), JsonRequestBehavior.AllowGet);
             jsonResult.MaxJsonLength = int.MaxValue;
