fc3510d476a658e6e952268ace97a718cc0e2f5c
David1010Gomez <dagomez.15m@gmail.com>
2017-12-27 17:04:11 -0500

    Commit

---
+++ Dime/Scripts/Midas/Tipificador.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+    $("#Li1").click(function () {
+        $("#Li2").css('background-color', 'transparent');
+        $("#Li1").css("background-color", "#dcdcdc");
+        $("#Li1").css("border-color", "#c23321");
+        $("#Li2").css("border-color", "transparent");
+    });
+    $("#Li2").click(function () {
+        $("#Li1").css('background-color', 'transparent');
+        $("#Li2").css("background-color", "#dcdcdc");
+        $("#Li2").css("border-color", "#c23321");
+        $("#Li1").css("border-color", "transparent");
+    });
+});
